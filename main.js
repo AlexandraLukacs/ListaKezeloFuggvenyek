c6c130cf32080b3d21432a93810d71847df0c80d
AlexandraLukacs <lszandika15@gmail.com>
2024-04-04 09:32:15 +0200

    kezeles

---
+++ main.js
@@ -0,0 +1,46 @@
+import { emberekLISTA } from "./adat.js";
+
+for (let index = 0; index < emberekLISTA.length; index++) {
+    const element = emberekLISTA[index];
+    console.log(element);
+}
+
+emberekLISTA.forEach((element, index) => { //első(element) a lista elemeit jelenti, második(index) a lista indexet
+    console.log(element);
+    console.log(index);
+});
+
+/* Szűrjük a férfiakra a listát */
+const FERFIAK = emberekLISTA.filter(function(ember){
+    //return ember.nem === false;
+    return !ember.nem  //ez az érték mindig logikai változó true/false
+})
+
+const FERFIAK2 = emberekLISTA.filter((ember) => {
+    //return ember.nem === false;
+    return !ember.nem  //ez az érték mindig logikai változó true/false
+})
+
+console.log(FERFIAK);
+
+
+/* Azokat az embereket szeretném, aki 20 évnél fiatalabb */
+const FIATALOK = emberekLISTA.filter(function(ember){
+    return ember.kor < 20;
+})
+
+console.log(FIATALOK)
+
+/* 10 évnél idősebb férfiak */
+const FERFIAK10FELETT = emberekLISTA.filter((ember) => {
+    return ember.kor > 10 && !ember.nem;
+})
+
+
+/* RENDEZÉS */
+/* Rendezzük fordított sorrendbe kor szerint az embereinket */
+emberekLISTA.sort(function(e1,e2){
+    return e2.kor-e1.kor;
+})
+
+console.log(emberekLISTA)
